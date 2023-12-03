fb40f4484ecc7fd6b6fc4fec1094dd33214a68fb
functionalStoic <7215306+functionalStoic@users.noreply.github.com>
2023-12-02 17:45:06 -0700

    feat(code): day 2, solution 2

--- src/day02/index.ts
+++ src/day02/index.ts
@@ -1,69 +1,89 @@
 import run from "aocrunner";
+import { readFileSync } from "fs";
+const inputFile = readFileSync("./src/day02/input.txt", "utf8");
 
 const parseInput = (rawInput: string) => rawInput;
 
-/**
- * Determine which games would have been possible if the bag had been loaded with only:
- * - 12 red cubes
- * - 13 green cubes
- * - 14 blue cubes
- */
 const part1 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  return parseInput(rawInput)
+    .split("\n")
+    .reduce((acc, line) => {
+      const [game, cubes] = line.split(": ");
+      const gameKey = game.split(" ")[1];
 
-  const lines = input.split("\n");
+      const games = cubes
+        .split("; ")
+        .map((subset) =>
+          subset.split(", ").reduce((acc, cube) => {
+            const [count, color] = cube.split(" ");
+            const parsedCount = parseInt(count);
 
-  const games = lines.reduce((acc, line) => {
-    const [game, cubes] = line.split(": ");
-    const gameKey = game.split(" ")[1];
+            switch (true) {
+              case color === "red" && parsedCount <= 12:
+              case color === "green" && parsedCount <= 13:
+              case color === "blue" && parsedCount <= 14:
+                return acc;
+              default:
+                return { ...acc, [color]: parsedCount };
+            }
+          }, {}),
+        )
+        .filter((game) => Object.keys(game).length > 0);
 
-    const games = cubes
-      .split("; ")
-      .map((subset) =>
-        subset.split(", ").reduce((acc, cube) => {
-          const [count, color] = cube.split(" ");
-          const parsedCount = parseInt(count);
+      return games.length > 0 ? acc : acc + parseInt(gameKey);
+    }, 0);
 
-          switch (true) {
-            case color === "red" && parsedCount <= 12:
-            case color === "green" && parsedCount <= 13:
-            case color === "blue" && parsedCount <= 14:
-              return acc;
-            default:
-              return { ...acc, [color]: parsedCount };
-          }
-        }, {}),
-      )
-      .filter((game) => Object.keys(game).length > 0);
-
-    return games.length > 0 ? acc : { ...acc, [gameKey]: games };
-  }, {});
-
-  return Object.keys(games).reduce((acc, key) => acc + parseInt(key), 0);
+  // return Object.keys(games).reduce((acc, key) => acc + parseInt(key), 0);
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+  return parseInput(rawInput)
+    .split("\n")
+    .reduce((acc, line) => {
+      const subsets = line.split(": ")[1];
 
-  return;
+      const gameOutcome = subsets.split("; ").map((subset) =>
+        subset.split(", ").reduce(
+          (acc, cube) => {
+            const [count, color] = cube.split(" ");
+            return { ...acc, [color]: parseInt(count) };
+          },
+          { red: 1, green: 1, blue: 1 },
+        ),
+      );
+
+      const maxRed = Math.max(...gameOutcome.map(({ red }) => red));
+      const maxGreen = Math.max(...gameOutcome.map(({ green }) => green));
+      const maxBlue = Math.max(...gameOutcome.map(({ blue }) => blue));
+
+      return acc + maxRed * maxGreen * maxBlue;
+    }, 0);
 };
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: `Game 1: 9 red, 2 green, 13 blue; 10 blue, 2 green, 13 red; 8 blue, 3 red, 6 green; 5 green, 2 red, 1 blue`,
-      //   expected: "",
-      // },
+      {
+        input: inputFile,
+        expected: 2283,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: inputFile,
+        expected: 78669,
+      },
+      {
+        input: `Game 1: 9 red, 2 green, 13 blue; 10 blue, 2 green, 13 red; 8 blue, 3 red, 6 green; 5 green, 2 red, 1 blue`,
+        expected: 1014,
+      },
+      {
+        input: `Game 95: 3 blue, 4 green, 10 red; 5 blue, 17 red, 2 green; 18 red, 2 green, 5 blue; 3 blue, 3 green, 2 red; 4 blue, 18 red; 6 red, 2 green`,
+        expected: 360,
+      },
     ],
     solution: part2,
   },
